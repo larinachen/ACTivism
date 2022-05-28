8273f27b06cbb46e1917cb5538fe675ae7ece32c
Larina Chen <larinachen2024@u.northwestern.edu>
2022-05-28 14:48:51 -0500

    init

--- app.py
+++ app.py
@@ -8,21 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def exercise1():
-    return '''
-        <!DOCTYPE html>
-        <html lang="en" >
-        <head>
-            <meta charset="UTF-8">
-            <title>Demo</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-            <link rel="stylesheet" href="/static/style.css">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        </head>
-        <body>
-            <h1>Hello </h1>
-        </body>
-        </html>
-    '''
+    return render_template(
+        'api/api_tester.html'
+    )
+    
 # enables flask app to run using "python3 app.py"
 if __name__ == '__main__':
     app.run()
